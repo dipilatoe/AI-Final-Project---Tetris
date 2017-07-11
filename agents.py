44b1b1716dd9f2de750d7152bd7067a2143a7385
Ethan DiPilato <dipilatoe@users.noreply.github.com>
2017-07-11 17:00:30 -0400

    added in successor and gamestate functions

--- agents.py
+++ agents.py
@@ -1,12 +1,19 @@
 import tetris
+from tetris import TetrisApp
 
 class Agents
 
-    def getGameState():
-        return , board
+    def getGameState(): #ED
+        return TetrisApp.stone_x, TetrisApp.stone_y, board, TetrisApp.next_stone
     
-    def getSuccessors():
-        util.raiseNotDefined()
+    def getSuccessors():    #ED
+        successors = []
+        if not tetris.check_collision(board, TetrisApp.stone, (TetrisApp.stone_x - 1, TetrisApp.stone_y)):
+            successors.append(tetris.join_matrices(TetrisApp.board, TetrisApp.stone, (TetrisApp.stone_x-1, TetrisApp.stone_y)))
+        if not tetris.check_collision(board, TetrisApp.stone, (TetrisApp.stone_x + 1, TetrisApp.stone_y)):
+            successors.append(tetris.join_matrices(TetrisApp.board, TetrisApp.stone, (TetrisApp.stone_x+1, TetrisApp.stone_y)))
+        #add in rotation successor
+        return successors
     
     def randomAgent():
         util.raiseNotDefined()
