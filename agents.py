31bf04a1ce716d436b815c5cd1546322811f2020
Ethan DiPilato <dipilatoe@users.noreply.github.com>
2017-07-11 14:58:29 -0400

    putting in some function definitions

---
+++ agents.py
@@ -0,0 +1,18 @@
+import tetris
+
+class Agents
+
+    def getGameState():
+        return , board
+    
+    def getSuccessors():
+        util.raiseNotDefined()
+    
+    def randomAgent():
+        util.raiseNotDefined()
+        
+    def greedyAgent():
+        util.raiseNotDefined()
+        
+    def optimalAgent():
+        util.raiseNotDefined()
