1950718193d7e2fb03142217ea0bc44afe542c41
Ethan DiPilato <dipilatoe@users.noreply.github.com>
2017-07-12 15:20:13 -0400

    renamed getSuccessors to generateSuccessor

--- agents.py
+++ agents.py
@@ -6,7 +6,7 @@ class Agents
     def getGameState(): #ED
         return TetrisApp.stone_x, TetrisApp.stone_y, board, TetrisApp.next_stone
     
-    def getSuccessors():    #ED
+    def generateSuccessor():    #ED
         successors = []
         if not tetris.check_collision(board, TetrisApp.stone, (TetrisApp.stone_x - 1, TetrisApp.stone_y)):
             successors.append(tetris.join_matrices(TetrisApp.board, TetrisApp.stone, (TetrisApp.stone_x-1, TetrisApp.stone_y)))
