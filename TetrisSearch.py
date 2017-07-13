a18c2d3d049eafd0bb38caf2b0fc5a9e2527e243
brettgrossman <grossmanb1@wit.edu>
2017-07-13 14:17:04 -0400

    Added note on generateSuccessor

--- TetrisSearch.py
+++ TetrisSearch.py
@@ -8,7 +8,7 @@ def getGameState():
 
 #generates a list of successors of potential states where the stone has moved left/right or rotated
 #CHANGE TO USE GAMESTATE INSTEAD OF DIRECTLY REFERENCING VARIABLES IN TETRIS.PY
-def generateSuccessor(self, gameState):
+def generateSuccessor(self, gameState, stone, action):
     successors = []
     if not tetris.check_collision(board, TetrisApp.stone, (TetrisApp.stone_x - 1, TetrisApp.stone_y)):
         successors.append(tetris.join_matrices(board, TetrisApp.stone, (TetrisApp.stone_x-1, TetrisApp.stone_y)))
@@ -17,6 +17,8 @@ def generateSuccessor(self, gameState):
     TetrisApp.rotate_stone
     successors.append(tetris.join_matrices(board, TetrisApp.stone, (TetrisApp.stone_x, TetrisApp.stone_y)))
     return successors
+
+#need a version of generateSuccessor that takes a single state, and an update to the state(piece and position the piece is going to go) and generates a new state
 
 #generates list of final positions
 #CHANGE TO USE GAMESTATE INSTEAD OF DIRECTLY REFERENCING VARIABLES IN TETRIS.PY
@@ -83,17 +85,17 @@ class ExpectimaxAgent():
 
 		#return action at top of tree
         if(currentDepth==0):
-            scoreHold, action = max([(self.value(state.generateSuccessor(TetrisApp.stone, x), depthLimit,1, 1),x) for x in actionList])
+            scoreHold, action = max([(self.value(generateSuccessor(state,TetrisApp.stone, x), depthLimit,1, 1),x) for x in actionList])
             return action
         #for previewed piece
 	    if(currentDepth==1):
-            return max(self.value(state.generateSuccessor(TetrisApp.next_stone, x), depthLimit, 2, 0 )for x in actionList)
+            return max(self.value(generateSuccessor(state,TetrisApp.next_stone, x), depthLimit, 2, 0 )for x in actionList)
 
 		#return score at max depth
         if(currentDepth==depthLimit and currentPiece is not 0):
-            return min(scoreEvaluationFunction(state.generateSuccessor(currentPiece,x) for x in actionList)
+            return min(scoreEvaluationFunction(generateSuccessor(state,currentPiece,x) for x in actionList)
 
 	    #all other cases (standard)
-        return max(self.value(state.generatesuccessor(currentPiece,x) depthLimit, currentDepth+1, 0) for x in actionList)
+        return max(self.value(generatesuccessor(state, currentPiece,x) depthLimit, currentDepth+1, 0) for x in actionList)
     def evaluationFunction(state):
         return state[4] * (1/len(getPieces.asList())
