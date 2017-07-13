a1f00504cd3f01c3743a9f899d7a0b7009a9a6d4
Ethan DiPilato <dipilatoe@users.noreply.github.com>
2017-07-13 13:51:57 -0400

    deleted agents.py and moved its contents into TetrisSearch.py

--- TetrisSearch.py
+++ TetrisSearch.py
@@ -2,6 +2,21 @@ from random import randint
 import tetris
 from tetris import TetrisApp
 
+#returns the base coordinate for the stone, the current state of the game board, and the next stone
+def getGameState():
+    return TetrisApp.stone_x, TetrisApp.stone_y, board, TetrisApp.next_stone
+
+#generates a list of successors of potential states where the stone has moved left/right or rotated
+def generateSuccessor():
+    successors = []
+    if not tetris.check_collision(board, TetrisApp.stone, (TetrisApp.stone_x - 1, TetrisApp.stone_y)):
+        successors.append(tetris.join_matrices(board, TetrisApp.stone, (TetrisApp.stone_x-1, TetrisApp.stone_y)))
+    if not tetris.check_collision(board, TetrisApp.stone, (TetrisApp.stone_x + 1, TetrisApp.stone_y)):
+        successors.append(tetris.join_matrices(board, TetrisApp.stone, (TetrisApp.stone_x+1, TetrisApp.stone_y)))
+    TetrisApp.rotate_stone
+    successors.append(tetris.join_matrices(board, TetrisApp.stone, (TetrisApp.stone_x, TetrisApp.stone_y)))
+    return successors
+
 #generates list of final positions
 def getActions():
     positions = []
